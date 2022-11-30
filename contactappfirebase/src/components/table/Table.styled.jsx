00e9c59c09e0b0528fe434c0c9d143fc358893d3
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-30 11:31:14 +0300

    v12

---
+++ contactappfirebase/src/components/table/Table.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TableStyled = styled.div`
+background-color: white;
+width : 30vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+
+`
--- contactappfirebase/src/components/table/table.jsx
+++ contactappfirebase/src/components/table/table.jsx
@@ -1,14 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { TableStyled } from "./Table.styled";
+import "./table.css";
+import { getDatabase, ref, onValue } from "firebase/database";
+import app from "../../utils/firebase";
+import DeleteIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from '@mui/icons-material/Edit';
 
 const Table = () => {
-  return (
-    <div>
-      <ul>
-        <li>1</li>
-        <li></li>
-      </ul>
-    </div>
-  )
-}
+  const [contactList, setContactList] = useState([]);
 
-export default Table
+  const handleGet = () => {
+    const database = getDatabase(app);
+    const constactRef = ref(database, "user/");
+
+    onValue(constactRef, (snapshot) => {
+      console.log(snapshot.val());
+    });
+  };
+
+  useEffect(() => {
+    const database = getDatabase(app);
+    const constactRef = ref(database, "user/");
+
+    onValue(constactRef, (snapshot) => {
+      const data = snapshot.val();
+      const contactArray = [];
+
+      for (let id in data) {
+        contactArray.push({ id, ...data[id] });
+        console.log(contactArray);
+      }
+      setContactList(contactArray);
+    });
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TableStyled>
+        <p>Contacts</p>
+
+        <table>
+          <thead>
+            <tr>
+              <th>user name</th>
+              <th>Phone Number</th>
+              <th>gender</th>
+              <th>delete</th>
+              <th>edit</th>
+            </tr>
+          </thead>
+          {contactList.map((item) => {
+            const { name, gender, phone } = item;
+            return (
+              <tbody>
+                <tr>
+                  <td>{name}</td>
+                  <td>{phone}</td>
+                  <td>{gender}</td>
+                  <td>
+                    <DeleteIcon />
+                  </td>
+                  <td>
+                    <EditIcon/>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </TableStyled>
+      <button onClick={handleGet}>get contacts</button>
+    </div>
+  );
+};
+
+export default Table;
